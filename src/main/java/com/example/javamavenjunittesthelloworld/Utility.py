4f07a10d047245e2f51c0b55d22d47ee7ceb35da
DongalaApoorva <apoorvareddydongala25@gmail.com>
2025-06-27 12:35:02 +0530

    APOORVA0001 - Wrote the code for finding the area of the circle

---
+++ src/main/java/com/example/javamavenjunittesthelloworld/Utility.py
@@ -0,0 +1,10 @@
+import math
+
+def apoorva():
+    print("I am Apoorva")
+
+def find_area_of_circle(radius):
+    return math.pi * radius * radius
+if __name__ == "__main__":
+    apoorva()
+    print("Area of circle:", find_area_of_circle(5))
